2429bd2511331c812bd01737c95cf179cc9b5de2
Sergey Gambaryan <serge_gamb@yahoo.com>
2025-01-10 16:44:48 +0300

    cosmetic refactoring

--- main.py
+++ main.py
@@ -30,40 +30,27 @@ async def main(request: Request):
         hook_params_dict[k] = v
     task_id = hook_params_dict['data[FIELDS_AFTER][ID]']
 
-    task_response = requests.get(
-            f'https://crm.agneko.com/rest/{BITRIX_SECRET}/tasks.task.get?taskId={task_id}'
-    ).json()
-    task = task_response['result']['task']
-    task_title = task['title']
-    task_description = task['description']
-    responsible_id = task['responsible']['id']
+    CRM_URL = f'https://crm.agneko.com/rest/{BITRIX_SECRET}'
 
-    user_response = requests.get(
-            f'https://crm.agneko.com/rest/{BITRIX_SECRET}/user.get.json?ID={responsible_id}'
-    ).json()
-    user_email = user_response_json['result'][0]['EMAIL']
-
-    task_item_response = requests.get(
-            f'https://crm.agneko.com/rest/{BITRIX_SECRET}/task.item.getdata?taskId={task_id}'
-    ).json()
-    uf_crm_task = task_item_response.get('result').get('UF_CRM_TASK')
+    task_response = requests.get(f'{CRM_URL}/tasks.task.get?taskId={task_id}')
+    task = task_response.json().get('result').get('task')
+    responsible_id = task.get('responsible').get('id')
+    user_response = requests.get(f'{CRM_URL}/user.get.json?ID={responsible_id}')
+    user_email = user_response.json().get('result')[0].get('EMAIL')
+    task_item_response = requests.get(f'{CRM_URL}/task.item.getdata?taskId={task_id}')
+    uf_crm_task = task_item_response.json().get('result').get('UF_CRM_TASK')
     lead_id = uf_crm_task[0].split('_')
-
-    lead_response = requests.get(
-            f'https://crm.agneko.com/rest/{BITRIX_SECRET}/crm.lead.get?id={lead_id}'
-    ).json()
-    contact_id = lead_response.get('result').get('CONTACT_ID')
-
-    contact_response = requests.get(
-            f'https://crm.agneko.com/rest/{BITRIX_SECRET}/crm.lead.get?id={lead_id}'
-    ).json()
-    contact_email = contact_response.get('result').get('EMAIL').get('VALUE')
+    lead_response = requests.get(f'{CRM_URL}/crm.lead.get?id={lead_id}')
+    contact_id = lead_response.json().get('result').get('CONTACT_ID')
+    contact_response = requests.get(f'{CRM_URL}/crm.lead.get?id={lead_id}')
+    contact_email = contact_response.json().get('result').get('EMAIL').get('VALUE')
+    
     print(contact_email)
 
     sdp_task = {
             'task': {
-                'title': task_title,
-                'description': task_description,
+                'title': task['title'],
+                'description': task['description'],
                 'owner': {
 
                     'email_id': user_email,
@@ -73,6 +60,7 @@ async def main(request: Request):
                 }
             }
     }
+
     params = {'input_data': json.dumps(sdp_task)}
     headers = {'authtoken': SC_TOKEN}
     sdp_task_response = requests.post(
